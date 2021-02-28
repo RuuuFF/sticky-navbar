53c575915b6e10d9526c58d7d8c320bda2b13048
Carlos Daniel <ruuufffaria@gmail.com>
2021-02-28 14:00:09 -0300

    Comentários

--- main.js
+++ main.js
@@ -8,3 +8,8 @@ function navChange() {
     nav.classList.remove('active')
   }
 }
+
+// window.scrollY - Retorna o número de pixels que o documento já rolou verticalmente.
+
+// A propriedade .offsetHeight é somente leitura e retorna um valor do tipo inteiro
+// a altura de um elemento incluindo padding-top + padding-bottom + border-top + border-bottom.
