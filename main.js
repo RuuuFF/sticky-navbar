c00b3c534eeffefc8751298dfa8029d9b2e1f153
Carlos Daniel <ruuufffaria@gmail.com>
2021-02-28 14:12:15 -0300

    comments update source

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ function navChange() {
 }
 
 // window.scrollY - Retorna o número de pixels que o documento já rolou verticalmente.
+// Mais: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/scrollY
 
 // A propriedade .offsetHeight é somente leitura e retorna um valor do tipo inteiro
 // a altura de um elemento incluindo padding-top + padding-bottom + border-top + border-bottom.
+// Mais: https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLElement/offsetHeight
