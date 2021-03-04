693540af37bf69c3c5fa77e3509e98284d92c53c
Carlos Daniel <ruuufffaria@gmail.com>
2021-03-04 20:22:54 -0300

    mobile menu

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const nav = document.querySelector('.nav')
-const navButtons = document.querySelector('.nav ul')
 
 window.addEventListener('scroll', navChange)
 
@@ -11,19 +10,27 @@ function navChange() {
   }
 }
 
-function toggleNav(el) {
-  navButtons.classList.toggle('appear')
-
-  if (navButtons.classList.contains('appear')) {
-    el.className = 'nav-toggle fas fa-times'
-  } else {
-    el.className = 'nav-toggle fas fa-bars'
-  }
-}
-
 // window.scrollY - Retorna o número de pixels que o documento já rolou verticalmente.
 // Mais: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/scrollY
 
 // A propriedade .offsetHeight é somente leitura e retorna um valor do tipo inteiro
 // a altura de um elemento incluindo padding-top + padding-bottom + border-top + border-bottom.
 // Mais: https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLElement/offsetHeight
+
+
+// function toggleNav(el) {
+//   navButtons.classList.toggle('appear')
+
+//   if (navButtons.classList.contains('appear')) {
+//     el.className = 'nav-toggle fas fa-times'
+//   } else {
+//     el.className = 'nav-toggle fas fa-bars'
+//   }
+// }
+
+const navToggle = document.getElementById('toggle-nav')
+const navButtons = document.getElementById('nav-buttons')
+
+navToggle.addEventListener('click', () => {
+  navButtons.classList.toggle('show')
+})
